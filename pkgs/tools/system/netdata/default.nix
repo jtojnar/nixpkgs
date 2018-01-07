415ca133a1a8e38f0596d4fe1c23d9edf24923e5
Jan Tojnar <jtojnar@gmail.com>
2018-01-07 05:32:14 +0100

    wip

--- pkgs/tools/system/netdata/default.nix
+++ pkgs/tools/system/netdata/default.nix
@@ -1,6 +1,16 @@
-{ stdenv, fetchFromGitHub, autoreconfHook, zlib, pkgconfig, libuuid }:
+{ stdenv, fetchFromGitHub, autoreconfHook, zlib, pkgconfig, libuuid, makeWrapper
+, withPython ? false, python3, withNode ? false, nodejs, withLmSensors ? false, lm_sensors }:
 
-stdenv.mkDerivation rec{
+let
+  inherit (stdenv.lib) optional optionalString;
+  # https://github.com/firehol/netdata/wiki/Installation#1-prepare-your-system
+  python = python3.withPackages (p: with p; [ pyyaml dnspython pymongo pymysql psycopg2 ]);
+  binDependencies =
+    optional withPython python
+    ++ optional withNode nodejs;
+  libDependencies =
+    optional withLmSensors lm_sensors;
+in stdenv.mkDerivation rec{
   version = "1.9.0";
   name = "netdata-${version}";
 
@@ -11,11 +21,13 @@ stdenv.mkDerivation rec{
     sha256 = "1vy0jz5lxw63b830l9jgf1qqhp41gzapyhdr5k1gwg3zghvlg10w";
   };
 
-  nativeBuildInputs = [ autoreconfHook pkgconfig ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig makeWrapper ];
   buildInputs = [ zlib libuuid ];
 
   # Allow UI to load when running as non-root
-  patches = [ ./web_access.patch ];
+  patches = [
+    ./web_access.patch
+  ];
 
   # Build will fail trying to create /var/{cache,lib,log}/netdata without this
   postPatch = ''
@@ -30,7 +42,18 @@ stdenv.mkDerivation rec{
   # The upstream installer does prepare an empty file too
   postInstall = ''
     touch $out/etc/netdata/netdata.conf
+
   '';
+
+  postFixup = ''
+    wrapProgram $out/bin/netdata \
+      --prefix PATH : "${stdenv.lib.makeBinPath binDependencies}" \
+      --prefix LD_LIBRARY_PATH : "${stdenv.lib.makeLibraryPath libDependencies}"
+  ''
+  # Remove unwanted plugins so that they cannot be accidentally enabled
+  + optionalString (!withPython) "rm -rf $out/libexec/netdata/python.d/*"
+  + optionalString (!withNode) "rm -rf $out/libexec/netdata/node.d/*"
+  + optionalString (!withLmSensors) "rm -f $out/libexec/netdata/python.d/sensors.chart.py";
 
   meta = with stdenv.lib; {
     description = "Real-time performance monitoring tool";
