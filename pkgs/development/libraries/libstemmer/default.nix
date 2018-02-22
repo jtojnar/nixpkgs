dba0c757a337a3f0d7e6d7e9ec7f933687c35287
Jan Tojnar <jtojnar@gmail.com>
2018-02-22 05:47:19 +0100

    libstemmer

--- pkgs/development/libraries/libstemmer/default.nix
+++ pkgs/development/libraries/libstemmer/default.nix
@@ -1,16 +1,28 @@
 { lib, stdenv, fetchFromGitHub, cmake }:
 
 stdenv.mkDerivation rec {
-  name = "libstemmer-2017-03-02";
+  name = "libstemmer-2017-06-16";
 
   src = fetchFromGitHub {
-    owner = "zvelo";
-    repo = "libstemmer";
+    owner = "snowballstem";
+    repo = "snowball";
     rev = "78c149a3a6f262a35c7f7351d3f77b725fc646cf";
     sha256 = "06md6n6h1f2zvnjrpfrq7ng46l1x12c14cacbrzmh5n0j98crpq7";
   };
 
-  nativeBuildInputs = [ cmake ];
+  patches = [
+    (fetchurl {
+      url = https://git.archlinux.org/svntogit/packages.git/plain/trunk/dynamiclib.patch?h=packages/snowball&id=e806c192ae0259003aba78f00fe7a276d6d05129;
+      sha256 = "0000000000000000000000000000000000000000000000000000000000000000";
+    })
+  ];
+
+  installPhase = ''
+    install -Dm644 {.,"$pkgdir"/usr}/include/libstemmer.h
+    install -D {.,"$pkgdir"/usr/lib}/libstemmer.so.0.0.0
+    ln -s libstemmer.so.0.0.0 "$pkgdir/usr/lib/libstemmer.so.0"
+    ln -s libstemmer.so.0 "$pkgdir/usr/lib/libstemmer.so"
+  '';
 
   meta = with lib; {
     description = "Snowball Stemming Algorithms";
