58273f6d139d36d26851a0f9630a3e31249f7aff
Jan Tojnar <jtojnar@gmail.com>
2018-03-05 19:32:28 +0100

    finalterm: switch to new fork

--- pkgs/applications/misc/finalterm/default.nix
+++ pkgs/applications/misc/finalterm/default.nix
@@ -1,6 +1,6 @@
-{ stdenv, fetchFromGitHub, makeWrapper
-, pkgconfig, cmake, ninja, libxml2, vala, gobjectIntrospection, intltool, gnome3, gtk3, gtk-doc
-, keybinder3, libnotify, json-glib
+{ stdenv, fetchFromGitHub, wrapGAppsHook
+, pkgconfig, meson, ninja, libxml2, vala, gobjectIntrospection, gettext, gnome3, gtk3, gtk-doc
+, keybinder3, libnotify, json-glib, glib, libunity
 , libxkbcommon, xorg, udev
 , bashInteractive
 }:
@@ -8,41 +8,29 @@
 with stdenv.lib;
 
 stdenv.mkDerivation {
-  name = "finalterm-git-2018-03-02";
+  name = "finalterm-unstable-2018-03-02";
 
   src = fetchFromGitHub {
-    owner = "RedHatter";
-    repo = "finalterm-reborn";
-    rev = "274a7ea232317a6cfe4e6a25eb68db38a111eb14";
-    sha256 = "1rpzxlja5rgs2g4nk69gkfnd5s5l7sz77r907zw1sixva47fyrnb";
+    owner = "finalterm";
+    repo = "finalterm";
+    rev = "64b80a8d1ddbc16972549b8123fe9fdba1b5ecb2";
+    sha256 = "0z20qkih6j4d5kvnm3mzrfr3325xpif15mfzdwxgmm0a3zzyc8g7";
   };
 
-  nativeBuildInputs = [ pkgconfig cmake ninja vala gobjectIntrospection intltool makeWrapper ];
+  nativeBuildInputs = [ pkgconfig meson ninja glib vala gobjectIntrospection gettext wrapGAppsHook ];
   buildInputs = [
-    gtk3 gnome3.libgee
+    glib gtk3 gnome3.libgee libunity
     gtk-doc keybinder3 libxml2 libnotify json-glib
   ] ++ optionals stdenv.isLinux [ udev ];
 
   preConfigure = ''
+    patchShebangs meson_post_install.py
     substituteInPlace data/org.gnome.finalterm.gschema.xml \
       --replace "/bin/bash" "${bashInteractive}/bin/bash"
-
-    cmakeFlagsArray=(
-      -DMINIMAL_FLAGS=ON
-    )
   '';
-
-  postInstall = ''
-    mkdir -p $out/share/gsettings-schemas/$name
-    mv $out/share/glib-2.0 $out/share/gsettings-schemas/$name/
-  '';
-
-  postFixup = ''
-    wrapProgram "$out/bin/finalterm" \
-      --prefix GI_TYPELIB_PATH : "$GI_TYPELIB_PATH" \
-      --prefix GIO_EXTRA_MODULES : "${getLib gnome3.dconf}/lib/gio/modules" \
-      --prefix XDG_DATA_DIRS : "${gnome3.defaultIconTheme}/share:${gnome3.gtk.out}/share:$out/share:$GSETTINGS_SCHEMAS_PATH"
-  '';
+  mesonFlags = [
+    "-Dlibnotify=true"
+  ];
 
   meta = {
     homepage = http://finalterm.org;
