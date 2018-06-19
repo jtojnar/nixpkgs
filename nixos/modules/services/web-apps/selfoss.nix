e5003d43a757260c8a896f6ced8be0f440f889a4
Jan Tojnar <jtojnar@gmail.com>
2018-06-19 13:48:16 +0200

    nixos/selfoss: update module

--- nixos/modules/services/web-apps/selfoss.nix
+++ nixos/modules/services/web-apps/selfoss.nix
@@ -8,49 +8,58 @@ let
 
   dataDir = "/var/lib/selfoss";
 
-  selfoss-config =
-  let
-    db_type = cfg.database.type;
-    default_port = if (db_type == "mysql") then 3306 else 5342;
-  in
-  pkgs.writeText "selfoss-config.ini" ''
-    [globals]
-    ${lib.optionalString (db_type != "sqlite") ''
-      db_type=${db_type}
-      db_host=${cfg.database.host}
-      db_database=${cfg.database.name}
-      db_username=${cfg.database.user}
-      db_password=${cfg.database.password}
-      db_port=${if (cfg.database.port != null) then cfg.database.port
-                    else default_port}
-    ''
-    }
-    ${cfg.extraConfig}
-  '';
-in
-  {
-    options = {
-      services.selfoss = {
-        enable = mkEnableOption "selfoss";
+  db_type = cfg.database.type;
+  default_port = if db_type == "mysql" then 3306 else 5342;
 
-        user = mkOption {
-          type = types.str;
-          default = "nginx";
-          example = "nginx";
-          description = ''
-            User account under which both the service and the web-application run.
-          '';
-        };
+  dbConfiguration =
+    if db_type == "sqlite" then {
+      SELFOSS_DB_FILE = "${dataDir}/sqlite/selfoss.db";
+    } else {
+      SELFOSS_DB_TYPE = db_type;
+      SELFOSS_DB_HOST = cfg.database.host;
+      SELFOSS_DB_DATABASE = cfg.database.name;
+      SELFOSS_DB_USERNAME = cfg.database.user;
+      SELFOSS_DB_PASSWORD = cfg.database.password;
+      SELFOSS_DB_PORT = if cfg.database.port != null then cfg.database.port else default_port;
+    };
 
-        pool = mkOption {
-          type = types.str;
-          default = "${poolName}";
-          description = ''
-            Name of existing phpfpm pool that is used to run web-application.
-            If not specified a pool will be created automatically with
-            default values.
-          '';
-        };
+  configurationEnvVars = {
+    SELFOSS_DATADIR = dataDir;
+    SELFOSS_BASEDIR = "${dataDir}/public";
+    SELFOSS_CACHE = "/tmp";
+    SELFOSS_LOGGER_DESTINATION = "error_log";
+  } // dbConfiguration // (mapAttrs' (name: value: nameValuePair ("SELFOSS_" + toUpper name) value) cfg.extraConfig);
+in {
+  options = {
+    services.selfoss = {
+      enable = mkEnableOption "selfoss";
+
+      user = mkOption {
+        type = types.str;
+        default = "nginx";
+        example = "nginx";
+        description = ''
+          User account under which both the service and the web-application run.
+        '';
+      };
+
+      pool = mkOption {
+        type = types.str;
+        default = "${poolName}";
+        description = ''
+          Name of existing phpfpm pool that is used to run web-application.
+          If not specified a pool will be created automatically with
+          default values.
+        '';
+      };
+
+      virtualHost = mkOption {
+        type = types.nullOr types.str;
+        default = "selfoss";
+        description = ''
+          Name of the nginx virtualhost to use and setup. If null, do not setup any virtualhost.
+        '';
+      };
 
       database = {
         type = mkOption {
@@ -105,8 +114,9 @@ in
         };
       };
       extraConfig = mkOption {
-        type = types.lines;
-        default = "";
+        type = types.attrsOf types.str;
+        default = {};
+        example = { items_perpage = "50"; };
         description = ''
           Extra configuration added to config.ini
         '';
@@ -116,7 +126,7 @@ in
 
   config = mkIf cfg.enable {
 
-    services.phpfpm.poolConfigs = mkIf (cfg.pool == "${poolName}") {
+    services.phpfpm.poolConfigs = mkIf (cfg.pool == poolName) {
       "${poolName}" = ''
         listen = "${phpfpmSocketName}";
         listen.owner = nginx
@@ -130,37 +140,70 @@ in
         pm.max_spare_servers = 20
         pm.max_requests = 500
         catch_workers_output = 1
+
+        php_value[variables_order] = EGPCS
+        ${concatStringsSep "\n" (mapAttrsToList (name: value: "env[${name}] = \"${value}\"") configurationEnvVars)}
       '';
     };
 
-    systemd.services.selfoss-config = {
-      serviceConfig.Type = "oneshot";
-      script = ''
-        mkdir -m 755 -p ${dataDir}
-        cd ${dataDir}
+    # NOTE: No configuration is done if not using virtual host
+    services.nginx = mkIf (cfg.virtualHost != null) {
+      enable = true;
+      virtualHosts = {
+        "${cfg.virtualHost}" = {
+          root = "${pkgs.selfoss}";
 
-        # Delete all but the "data" folder
-        ls | grep -v data | while read line; do rm -rf $line; done || true
+          locations."/" = {
+            index = "index.php";
+            tryFiles = "$uri /public/$uri /index.php$is_args$args";
+          };
 
-        # Create the files
-        cp -r "${pkgs.selfoss}/"* "${dataDir}"
-        ln -sf "${selfoss-config}" "${dataDir}/config.ini"
-        chown -R "${cfg.user}" "${dataDir}"
-        chmod -R 755 "${dataDir}"
-      '';
-      wantedBy = [ "multi-user.target" ];
+          locations."~ \\.php$" = {
+            extraConfig = ''
+              fastcgi_split_path_info ^(.+\.php)(/.+)$;
+              fastcgi_pass unix:${phpfpmSocketName};
+              fastcgi_index index.php;
+              fastcgi_param SCRIPT_FILENAME $document_root/$fastcgi_script_name;
+              include ${pkgs.nginx}/conf/fastcgi_params;
+            '';
+          };
+          locations."~* \\ (gif|jpg|png)" = {
+            extraConfig = ''
+              expires 30d;
+            '';
+          };
+          locations."~ ^/(favicons|thumbnails)/.*$" = {
+            extraConfig = ''
+              try_files $uri /data/$uri;
+            '';
+          };
+          locations."~* ^/(data\\/logs|data\\/sqlite|config\\.ini|\\.ht)" = {
+            extraConfig = ''
+              deny all;
+            '';
+          };
+          extraConfig = ''
+            error_log syslog:server=unix:/dev/log debug;
+            access_log syslog:server=unix:/dev/log;
+            access_log on;
+          '';
+        };
+      };
     };
 
     systemd.services.selfoss-update = {
       serviceConfig = {
-        ExecStart = "${pkgs.php}/bin/php ${dataDir}/cliupdate.php";
+        ExecStart = "${pkgs.php}/bin/php ${pkgs.selfoss}/cliupdate.php";
         User = "${cfg.user}";
       };
       startAt = "hourly";
       after = [ "selfoss-config.service" ];
       wantedBy = [ "multi-user.target" ];
-
+      environment = configurationEnvVars;
     };
 
+    systemd.tmpfiles.rules = [
+      "d ${dataDir} 0755 ${cfg.user} nginx -"
+    ];
   };
 }
--- pkgs/servers/web-apps/selfoss/default.nix
+++ pkgs/servers/web-apps/selfoss/default.nix
@@ -5,9 +5,14 @@ stdenv.mkDerivation rec {
   version = "2.18";
 
   src = fetchurl {
-    url = "https://github.com/SSilence/selfoss/releases/download/${version}/${name}.zip";
-    sha256 = "1vd699r1kjc34n8avggckx2b0daj5rmgrj997sggjw2inaq4cg8b";
+    url = https://bintray.com/fossar/selfoss/download_file?file_path=selfoss-2.19-cdd9125.zip;
+    sha256 = "11l5nryp6iw1jrkmx7zg77dfcnva85dz1z99fr439rjrcv12k8xn";
   };
+
+  patches = [
+    /home/jtojnar/Projects/selfoss/1043.patch
+    /home/jtojnar/Projects/selfoss/moo2.patch
+  ];
 
   sourceRoot = ".";
   nativeBuildInputs = [ unzip ];
