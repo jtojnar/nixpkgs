ffb87abe8c68a0179934e73cb0a6f341c7bf79e1
Jan Tojnar <jtojnar@gmail.com>
2020-09-11 01:13:36 +0200

    hspell: clean up
    - move pname & version to derivation proper to make it neater
    - move perl to native build inputs since it only seems to be needed at build time
    - reduce scope of patchShebangs and add explicit --build
    - change patchPhase to postPatch to avoid clash with patches
    - correct license to AGPL (not sure if -or-later or -only so falling back to the latter)
    - add comment
    - enable strictDeps to enforce {native,} inputs split
    - format the args a bit
    - change hspellDicts appropriately and unbreak them

--- pkgs/development/libraries/hspell/default.nix
+++ pkgs/development/libraries/hspell/default.nix
@@ -1,27 +1,41 @@
-{ stdenv, fetchurl, perl, zlib }:
+{ stdenv
+, fetchurl
+, perl
+, zlib
+}:
 
 stdenv.mkDerivation rec {
-  name = "${passthru.pname}-${passthru.version}";
-
-  passthru = {
-    pname = "hspell";
-    version = "1.1";
-  };
-
-  PERL_USE_UNSAFE_INC = "1";
+  pname = "hspell";
+  version = "1.1";
 
   src = fetchurl {
-    url = "${meta.homepage}${name}.tar.gz";
+    url = "${meta.homepage}${pname}-${version}.tar.gz";
     sha256 = "08x7rigq5pa1pfpl30qp353hbdkpadr1zc49slpczhsn0sg36pd6";
   };
 
-  patchPhase = ''patchShebangs .'';
-  buildInputs = [ perl zlib ];
+  nativeBuildInputs = [
+    perl
+  ];
+
+  buildInputs = [
+    zlib
+  ];
+
+  strictDeps = true;
+
+  # Can't locate PrefixBits.pl in @INC […] at ./pmerge line 11.
+  PERL_USE_UNSAFE_INC = "1";
+
+  postPatch = ''
+    patchShebangs --build \
+      pmerge \
+      wzip
+  '';
 
   meta = with stdenv.lib; {
     description = "Hebrew spell checker";
     homepage = "http://hspell.ivrix.org.il/";
     platforms = platforms.all;
-    license = licenses.gpl2;
+    license = licenses.agpl3Only;
   };
 }
--- pkgs/development/libraries/hspell/dicts.nix
+++ pkgs/development/libraries/hspell/dicts.nix
@@ -1,15 +1,13 @@
 { stdenv, hspell }:
 
 let
-  dict = variant: a: stdenv.mkDerivation ({
-    inherit (hspell) src patchPhase nativeBuildInputs;
+  dict = variant: a: hspell.overrideAttrs (orig: a // {
     buildFlags = [ variant ];
 
-    meta = hspell.meta // {
-      broken = true;
+    meta = orig.meta // {
       description = "${variant} Hebrew dictionary";
-    } // (if a ? meta then a.meta else {});
-  } // (removeAttrs a ["meta"]));
+    } // (a.meta or {});
+  });
 in
 {
   recurseForDerivations = true;
@@ -18,23 +16,32 @@ in
     name = "aspell-dict-he-${hspell.version}";
 
     installPhase = ''
+      runHook preInstall
       mkdir -p $out/lib/aspell
-      cp -v he_affix.dat he.wl $out/lib/aspell'';
+      cp -v he_affix.dat he.wl $out/lib/aspell
+      runHook postInstall
+    '';
   };
 
   myspell = dict "myspell" {
     name = "myspell-dict-he-${hspell.version}";
 
     installPhase = ''
+      runHook preInstall
       mkdir -p $out/lib/myspell
-      cp -v he.dic he.aff $out/lib/myspell'';
+      cp -v he.dic he.aff $out/lib/myspell
+      runHook postInstall
+    '';
   };
 
   hunspell = dict "hunspell" {
     name = "hunspell-dict-he-${hspell.version}";
 
     installPhase = ''
+      runHook preInstall
       mkdir -p $out/lib
-      cp -rv hunspell $out/lib'';
+      cp -rv hunspell $out/lib
+      runHook postInstall
+    '';
   };
 }
