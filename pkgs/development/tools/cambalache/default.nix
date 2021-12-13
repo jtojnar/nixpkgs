260627078566f3753dd264a15a92efc666516f5e
Dmitriy <43755002+psydvl@users.noreply.github.com>
2021-12-13 20:30:40 +0300

    Fix broadwayd
    Switch to release
    Fix license

--- pkgs/development/tools/cambalache/default.nix
+++ pkgs/development/tools/cambalache/default.nix
@@ -1,33 +1,29 @@
-{ stdenv
-, lib
-, fetchFromGitLab
+{ stdenv, lib, fetchFromGitLab
 , python3
-, meson
-, ninja
-, pkg-config
+, meson, ninja, pkg-config
 , gobject-introspection
-, desktop-file-utils
-, shared-mime-info
+, desktop-file-utils, shared-mime-info
 , wrapGAppsHook
-, glib
-, gtk3
-, gtk4
+, appstream-glib , gtk3, gtk4
 , webkitgtk
-, unstableGitUpdater
+, genericUpdater
+, useGTK3Broadwayd ? false
+, useGTK4Broadwayd ? false
 }:
 
 python3.pkgs.buildPythonApplication rec {
   pname = "cambalache";
-  version = "unstable-2021-09-23";
+  version = "0.8.0";
 
   format = "other";
+  strictDeps = false; # https://github.com/NixOS/nixpkgs/issues/56943
 
   src = fetchFromGitLab {
     domain = "gitlab.gnome.org";
     owner = "jpu";
     repo = pname;
-    rev = "69b39dedbf9ccb3fac6afaa8f533d79e68d36792";
-    sha256 = "X+VOZQicUUrHsXm+ttLAPrtc94pvTdjDI8hIe/Zd3ks=";
+    rev = version;
+    sha256 = "05hf45v24l73fwv36bf90h6ksf6axvi8f4mddyq24x63w1yndhg9";
   };
 
   nativeBuildInputs = [
@@ -40,20 +36,17 @@ python3.pkgs.buildPythonApplication rec {
     wrapGAppsHook
   ];
 
-  pythonPath = with python3.pkgs; [
+  propagatedBuildInputs = with python3.pkgs; [
     pygobject3
     lxml
   ];
 
   buildInputs = [
-    glib
+    appstream-glib
     gtk3
     gtk4
     webkitgtk
   ];
-
-  # Not compatible with gobject-introspection setup hooks.
-  strictDeps = false;
 
   # Prevent double wrapping.
   dontWrapGApps = true;
@@ -65,29 +58,34 @@ python3.pkgs.buildPythonApplication rec {
   preFixup = ''
     # Let python wrapper use GNOME flags.
     makeWrapperArgs+=(
+      # For gtk3 broadwayd
+      ${ lib.optionalString useGTK3Broadwayd ''--prefix PATH : "${gtk3}/bin"''}
       # For gtk4-broadwayd
-      --prefix PATH : "${gtk4.dev}/bin"
+      ${ lib.optionalString useGTK4Broadwayd ''--prefix PATH : "${gtk4.dev}/bin"''}
       "''${gappsWrapperArgs[@]}"
     )
   '';
 
   postFixup = ''
+    mkdir -p $out/bin
+    ${ lib.optionalString (!useGTK3Broadwayd) "install ${gtk3}/bin/broadwayd $out/bin/"}
+    ${ lib.optionalString (!useGTK4Broadwayd) "install ${gtk4.dev}/bin/gtk4-broadwayd $out/bin/"}
     # Wrap a helper script in an unusual location.
     wrapPythonProgramsIn "$out/${python3.sitePackages}/cambalache/priv/merengue" "$out $pythonPath"
   '';
 
   passthru = {
-    updateScript = unstableGitUpdater {
+    updateScript = genericUpdater {
       url = "${meta.homepage}.git";
     };
   };
 
   meta = with lib; {
     homepage = "https://gitlab.gnome.org/jpu/cambalache";
-    description = "RAD tool for GTK 4 and 3";
+    description = "RAD tool for Gtk 4 and 3 with a clear MVC design and data model first philosophy.";
     maintainers = teams.gnome.members;
     license = with licenses; [
-      lgpl2Only # Cambalache
+      lgpl21Only # Cambalache
       gpl2Only # tools
     ];
     platforms = platforms.unix;
